a2576eae060eb39762cbe5ceb7f9cde0f002931a
aisehiwebwala <116726737+aisehiwebwala@users.noreply.github.com>
2024-07-21 00:47:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use("/file", express.static(__dirname + "/file"));
+app.use("/file", express.static("/tmp"));
 app.use(express.static(__dirname+"/dist"))
 
 app.get("/upload-file", (req, res) => {
@@ -14,7 +14,7 @@ app.get("/upload-file", (req, res) => {
 });
 
 app.get("/files",async(req,res)=>{
-    const list = fs.readdirSync(__dirname+"/file",{withFileTypes:true});
+    const list = fs.readdirSync("/tmp",{withFileTypes:true});
     let html = "";
     for(let file of list){
       html+=`<a style="display:block;margin:5px;" href="/file/${file.name}" target="_blank"> ${file.name} </a>`
@@ -26,12 +26,12 @@ app.post("/upload-file", upload.single("phile"), async (req, res) => {
   try {
     const file_info = JSON.parse(req.body.file_info);
     
-    if(file_info.is_first && fs.existsSync(__dirname+"/file/"+file_info.file_name)){
-      fs.rmSync(__dirname+"/file/"+file_info.file_name);
+    if(file_info.is_first && fs.existsSync("/tmp/"+file_info.file_name)){
+      fs.rmSync("/tmp/"+file_info.file_name);
     }
 
     const file = req.file;
-    fs.appendFileSync(__dirname + "/file/" + file_info.file_name, file.buffer);
+    fs.appendFileSync("/tmp/" + file_info.file_name, file.buffer);
     res.status(200).send("OK");
 
   } catch (error) {
